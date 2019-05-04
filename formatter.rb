c6cecc71ce638c05cb082ca2e8fb490cd1d337d9
Padeen <paulo.deen@gmail.com>
2019-05-04 03:57:42 +0200

    Add comments to formatter

--- formatter.rb
+++ formatter.rb
@@ -5,7 +5,9 @@ module Formatter
     end
 
     def formatPrice(price)
+        # remove point for thousands notation
         priceFormatted1 = price.gsub(/\./, "")
+        # change to standard dot notation for decimals
         priceFormatted2 = priceFormatted1.gsub(/,/, ".")
     end
 end
